e0a09aa2bbc69d79974deabafeb4cd99b0f3af55
Sebastian Schepis <sschepis@gmail.com>
2013-10-24 12:06:24 -0700

    Tweaked loader so that it actually works with JWPlayer

--- jwplayer-loader.js
+++ jwplayer-loader.js
@@ -1,6 +1,7 @@
 //Functions to run after the script tag has loaded
 var jwplayerLoadCallback = function(){
-
+	if(!jwplayer)console.log("JWPlayer not found!");	
+	else jwplayer.key = "9IQoJNofYuFbhrJGURs9NEz/4+OiKpGoJ0ADpHXSCw8=";
 };
 
 //If the script doesn't load
@@ -15,8 +16,11 @@ var head = document.getElementsByTagName('head')[0];
 //Generate a script tag
 var script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = '//jwpsrv.com/library/U4WWQDLeEeOytRIxOUCPzg.js';
+script.src = '/js/jwplayer/jwplayer.js';
+script.id = 'jwplayerscript';
+script.base = 'http://10.0.1.134:3000/js/jwplayer/'; // TODO programmatic base URL
 script.onload = jwplayerLoadCallback;
 script.onerror = jwplayerErrorCallback;
 
 head.appendChild(script);
+
